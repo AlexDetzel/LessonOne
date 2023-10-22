87aea6ccfc18e8b5e7bea98315fbd8ec679eb870
AlexDetzel <wwwdetzel@gmail.com>
2023-10-22 14:25:16 +0700

    Тестирование работы модели представления

--- LessonOne/ViewModels/MainWindowViewModel.cs
+++ LessonOne/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using LessonOne.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LessonOne.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Zagolovok okna
+        private string _Title = "Мой первый проект";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (Equals(_Title, value)) return;
+                _Title = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
     }
 }
